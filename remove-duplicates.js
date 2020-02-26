53b1da76018665b50781945c19fc5ab8fd4071a7
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-02-26 09:35:34 +0100

    cleaning

--- remove-duplicates.js
+++
@@ -1,8 +0,0 @@
-const neek = require('neek');
-
-const readable = './responses-not-equal.txt';
-const writable = './responses-not-equal-no-duplicates.txt';
-
-neek.unique(readable, writable, function(result){
-    console.log(result);
-});
